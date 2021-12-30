377940f5ce109155f04c4a0fe0cd3722373092b4
Davis Gossage <davisgossage@gmail.com>
2021-12-30 09:07:05 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,5 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "DiffMatchPatch",
-    targets: [Target(name: "DiffMatchPatch", dependencies:["diff_match_patch"])]
+    products: [
+        .library(
+            name: "DiffMatchPatch",
+            targets: ["DiffMatchPatch"]),
+    ],
+    targets: [
+        .target(
+            name: "DiffMatchPatch",
+            path: "Sources"),
+    ]
 )
